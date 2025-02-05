14ccdf5164cde5981191520a490dc4f6f30c7ac4
Avila-Pato <p.avilaf1998@gmail.com>
2025-02-05 14:07:19 -0300

    feat: enhance task detail and list views; implement task update and delete functionality

--- frontend/src/views/TaskDetail.vue
+++ frontend/src/views/TaskDetail.vue
@@ -1,5 +1,67 @@
-<script setup></script>
-
 <template>
-  <h1>Task Detail</h1>
+  <div>
+    <h1>Detalles de la Tarea</h1>
+    <form @submit.prevent="handleUpdate()">
+      <!-- Mostrar el título y la descripción de la tarea seleccionada -->
+      <input type="text" v-model="currentTask.title" placeholder="" />
+      <textarea v-model="currentTask.description" placeholder="Descripción de la tarea"></textarea>
+      <button>Actualizar</button>
+    </form>
+
+    <button @click="handleDelete()">Borrar</button>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Task } from '../types/Task'
+import { deleteTask, getTask, updateTask } from '../services/taskService'
+
+export default defineComponent({
+  name: 'task',
+  data() {
+    return {
+      currentTask: {} as Task, // Aquí se almacenará la tarea que vamos a editar
+    }
+  },
+  methods: {
+    async loadTask(id: string) {
+      try {
+        // Obtener la tarea desde la API usando el ID
+        const { data } = await getTask(id)
+        this.currentTask = data // Cargar los datos en currentTask
+        console.log('Tarea obtenida', data)
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    async handleUpdate() {
+      try {
+        if (typeof this.$route.params.id === 'string') {
+          // Actualizar la tarea con los datos modificados
+          await updateTask(this.$route.params.id, this.currentTask)
+          this.$router.push('/') // Redirigir a la lista de tareas
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    async handleDelete() {
+      try {
+        if (typeof this.$route.params.id === 'string') {
+          await deleteTask(this.$route.params.id)
+          this.$router.push('/') // Redirigir a la lista de tareas
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    mounted() {
+      if (typeof this.$route.params.id === 'string') {
+        // Cargar la tarea al montar el componente
+        this.loadTask(this.$route.params.id)
+      }
+    },
+  },
+})
+</script>
--- frontend/src/views/TaskList.vue
+++ frontend/src/views/TaskList.vue
@@ -4,19 +4,18 @@
       class="list-group-item list-group-item-action p-4"
       style="cursor: pointer"
       v-for="(task, index) in tasks"
-      :key="index"
+      :key="task._id"
       @click="this.$router.push(`/tasks/${task._id}`)"
     >
-      {{ index + 1 }}.
-      {{ task.title }}
+      {{ index + 1 }}. {{ task.title }}
     </li>
   </ul>
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue'
 import { Task } from '../types/Task'
 import { getTasks } from '../services/taskService'
-import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'tasks-list',
@@ -30,7 +29,6 @@ export default defineComponent({
       try {
         const res = await getTasks()
         this.tasks = res.data
-        console.log('Tarea actualizada correctamente', res)
       } catch (error) {
         console.error(error)
       }
